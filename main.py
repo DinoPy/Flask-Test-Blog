2fdc17c1a808ebdc4130c4b5a153ab9fa3f26e62
DinoOne <001test.alex@gmail.com>
2021-02-07 23:45:35 +0200

    Complete version of authentication flask blog with gunicorn with something else idk

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+from datetime import date
+from functools import wraps
+import os
 from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
-from datetime import date
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm,RegisterForm , LoginForm,CommentForm
-from flask_gravatar import Gravatar
-from functools import wraps
-from flask import abort
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'blablakey12311132'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
